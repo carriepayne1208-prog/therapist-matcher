eaa10cc844d12114fc99e043baa05578effb9acb
carriepayne1208-prog <carriepayne1208@gmail.com>
2025-08-20 11:46:30 -0400

    Update and rename sup abase.ts to supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -15,7 +15,6 @@
 /app/api/consults/create/route.ts
 // /lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
-
 export const supabaseAnon = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
